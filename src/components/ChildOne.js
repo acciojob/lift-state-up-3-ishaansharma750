87faddbee1085d5bf1226295cf9f19c8e27b3a32
Suansh Budhiraja <ishaansharma750@gmail.com>
2023-08-03 01:10:18 +0530

    Third commit

--- src/components/ChildOne.js
+++ src/components/ChildOne.js
@@ -5,7 +5,7 @@ const ChildTwo = (prop) => {
         <div className="child-two">
             {console.log(prop)}
             <h2>Child Component 2</h2>
-            <button onClick={() => prop.change('Option 2')}>Option 2</button>
+            <button onClick={() => prop.change('Option 2')}>Option 1</button>
         </div>
     )
 }
